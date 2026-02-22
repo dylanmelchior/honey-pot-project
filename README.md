90b976d9660cb6aa8e2b6fd776a3deae4374b399
Dylan Melchior <dmelchior39@gmail.com>
2026-02-22 13:53:46 -0700

    Honeypot is functional and has been tested. Updated README with ethical considerations and project overview.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # honey-pot-project
+
+## Overview
+- This project is a simple honeypot server. It currently can only handle one connection at a time listening on port 4444. It appends to a simple log file with the client IP Address, source port, and destination port. It has room for expansion to create an elaborate honeypot server. Dependencies are as follows: 
+- netinet/in.h
+- arpa/inet.h
+- sys/socket.h
+- unistd.h
+- cstring
+- iostream
+- fstream
+
+- The honeypot must be ran on an Ubunutu host. This tool is not for production usage, and is merely a development/practice tool.
+
+## Ethical Considerations and Misuse
+- This tool is a defensive tool and thus misuage is unlikely. It does give a false sense of security, as 
+this tool is not designed for professional use and should not be used in a production environment. It is likely it could be modified for malicious purposes, although I am unaware as to how that would be done.
+
+
+## References:
+- https://www.geeksforgeeks.org/cpp/socket-programming-in-cpp/
+- https://www.man7.org/linux/man-pages/man2/listen.2.html
+- https://www.man7.org/linux/man-pages/man2/accept.2.html
+- https://www.baeldung.com/cs/socket-api-accept
+- https://learn.microsoft.com/en-us/windows/win32/winsock/sockaddr-2
+- https://cplusplus.com/doc/tutorial/files/
+- https://stackoverflow.com/questions/5328070/how-to-convert-string-to-ip-address-and-vice-versa
+- https://www.geeksforgeeks.org/cpp/opening-modes-in-standard-i-o-in-c-c-with-examples/
+- Utilized Claude LLM for brainstorming project ideas and potential project expansions outside of this     assignment.
--- honeypot.cpp
+++ honeypot.cpp
@@ -2,24 +2,29 @@
 This class represents a basic honeypot listening and logging connections on port 4444.
 @author Dylan Melchior
 
-REFERENCES 
-https://www.geeksforgeeks.org/cpp/socket-programming-in-cpp/
-https://www.man7.org/linux/man-pages/man2/listen.2.html
-https://www.man7.org/linux/man-pages/man2/accept.2.html
-https://www.baeldung.com/cs/socket-api-accept
-
 */
 
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 
 using std::cout;
 using std::endl;
+using std::ofstream;
 
 int main(){
+
+    ofstream logFile;
+    logFile.open("connectionLog.txt", std::ios::app);
+
+    if (!(logFile.is_open())){
+        cout << "Could not open log file." << endl;
+        return -1;
+    }
 
     int honeypotSocket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -33,6 +38,11 @@ int main(){
     honeypotAddress.sin_port = htons(4444);
     honeypotAddress.sin_addr.s_addr = INADDR_ANY;
 
+    const int enableSocketReuse = 1;
+    if (setsockopt(honeypotSocket, SOL_SOCKET, SO_REUSEADDR, &enableSocketReuse, sizeof(int)) < 0){
+        cout << "Socket could not be reused" << endl;
+    }
+    
     if (bind(honeypotSocket, (struct sockaddr*)&honeypotAddress, sizeof(honeypotAddress)) == -1){
         cout << "Could not bind socket" << endl;
         return -1;
@@ -45,16 +55,21 @@ int main(){
     }
 
     while (true){
+        sockaddr_in clientAddress;
+        socklen_t clientAddressLen = sizeof(clientAddress);
+        int clientSocket = accept(honeypotSocket, (struct sockaddr*)&clientAddress, &clientAddressLen);
 
-        int clientSocket = accept(honeypotSocket, nullptr, nullptr);
-        if (!(clientSocket < 0)){
-            
+        if (clientSocket > 0){
+            char clientIP[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &clientAddress.sin_addr, clientIP, INET_ADDRSTRLEN);
+            logFile <<  clientIP << "|";
+            logFile << ntohs(clientAddress.sin_port) << "|";
+            logFile << "4444" << endl;
+            close(clientSocket);
         }
         else {
-
+            cout << "Could not accept client connection." << endl;
         }
-
     }
-
-
+    logFile.close();
 }
